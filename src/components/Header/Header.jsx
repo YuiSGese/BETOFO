007cb9da0002a46e4d6d4074d94d5900b38cdbbd
yuiSGese <yuivnch@gmail.com>
2024-12-16 22:26:16 +0900

    Frontend Done

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header-contents">
+        <h2>
+          ベトナム <br /> 料理なら
+        </h2>
+        <h1>BETOFO</h1>
+
+        <button>View Menu</button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
